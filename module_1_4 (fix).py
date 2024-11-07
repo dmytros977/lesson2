06bdfb94da1cc5041052fc7f3738594dd66f238e
Dima <72711928+dmytros977@users.noreply.github.com>
2024-11-07 16:29:08 +0300

    Add files via upload
    была  ошибка в пеерменных

---
+++ module_1_4 (fix).py
@@ -0,0 +1,10 @@
+homework_numb = 12
+print(homework_numb)
+all_time = 1.5
+print(all_time)
+course_of_study = 'Python'
+print(course_of_study)
+print(all_time / homework_numb)
+spend_time =0.125
+print('Курс:', course_of_study, 'всего задач:', homework_numb, ',затрачено часов', all_time, ',среднее время выполнения', spend_time, 'часа.')
+
