af08fbbf4cad2fea669556d4600bb534ba9deeab
Dima <72711928+dmytros977@users.noreply.github.com>
2024-11-07 16:23:55 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+homework_numb = 12
+print(homework_numb)
+time = 1.5
+print(time)
+course_of_study = 'Python'
+print(course_of_study)
+print(time / homework_numb)
+spend_time =0.125
+print('Курс:', course_of_study, 'всего задач:', homework_numb, ',затрачено часов', spend_time, ',среднее время выполнения', spend_time, 'часа.')
+
